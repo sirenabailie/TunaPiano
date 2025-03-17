b09e7e12e8407cca10a8ff925dc1ad2b2963b631
sirenabailie <sirenabailie@gmail.com>
2025-03-17 18:43:29 -0500

    Created Models

---
+++ Models/Artist.cs
@@ -0,0 +1,12 @@
+﻿namespace Tuna_Piano.Models
+{
+    public class Artist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Bio {  get; set; }
+
+        public List<Song> Songs { get; set;}
+    }
+}
